24bcee58ad2e2af77226a88a2b18484e5c149f7d
steduq10 <steven.duque.95@gmail.com>
2022-05-02 10:55:55 -0500

    Se crea el intro

---
+++ CalculadoraTienda.py
@@ -0,0 +1,6 @@
+print("*****************************")
+print("    Iniciando Calculadora    ")
+print("*****************************")
+
+valorUnitario = int(input("Por favor, ingrese el valor unitario del producto (sin IVA incluido): "))
+
