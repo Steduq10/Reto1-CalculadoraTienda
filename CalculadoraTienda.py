0957c8f75be48f42de031f6bf0a02191730fd3ab
steduq10 <steven.duque.95@gmail.com>
2022-05-02 14:49:54 -0500

    Configuración de los condicionales y ciclos para el funcionamiento de la calculadora dado si el producto tiene IVA o no

--- CalculadoraTienda.py
+++ CalculadoraTienda.py
@@ -6,12 +6,60 @@ valorUnitario = int(input("Por favor, ingrese el valor unitario del producto (si
 
 iva = input("¿El producto tiene IVA? S/N: ")
 
-while iva == "S":
+if  iva == "S":
     cantidad = int(input("Ingrese la cantidad del producto: "))
     subtotal = (valorUnitario*cantidad) + (cantidad*valorUnitario*0.19)
     print(f"SUBTOTAL: {subtotal} (IVA incluido)")
-    
-while iva == "N":
+    lista = [subtotal]
+    print(lista)
+    agregar = input("Faltan productos por agregar? S/N:")
+    while agregar == "S":
+        valorUnitario = int(input("Por favor, ingrese el valor unitario del producto (sin IVA incluido): "))
+        iva = input("¿El producto tiene IVA? S/N: ")
+        if iva == "S":
+            cantidad = int(input("Ingrese la cantidad del producto: "))
+            subtotal = (valorUnitario * cantidad) + (cantidad * valorUnitario * 0.19)
+            lista.append(subtotal)
+            print(f"SUBTOTAL: {subtotal} (IVA incluido)")
+            print(lista)
+            agregar = input("Faltan productos por agregar? S/N:")
+        elif iva == "N":
+            cantidad = int(input("Ingrese la cantidad del producto: "))
+            subtotal = (valorUnitario * cantidad)
+            lista.append(subtotal)
+            print(f"SUBTOTAL: {subtotal} (IVA incluido)")
+            print(lista)
+            agregar = input("Faltan productos por agregar? S/N:")
+        else:
+            agregar == "N"
+
+
+elif iva == "N":
     cantidad = int(input("Ingrese la cantidad del producto: "))
     subtotal = (valorUnitario * cantidad)
-    print(f"SUBTOTAL: {subtotal} (PRODUCTO SIN IVA)")
+    lista = [subtotal]
+    print(f"SUBTOTAL: {subtotal} (IVA incluido)")
+    print(lista)
+    agregar = input("Faltan productos por agregar? S/N:")
+    while agregar == "S":
+        valorUnitario = int(input("Por favor, ingrese el valor unitario del producto (sin IVA incluido): "))
+        iva = input("¿El producto tiene IVA? S/N: ")
+        if iva == "S":
+            cantidad = int(input("Ingrese la cantidad del producto: "))
+            subtotal = (valorUnitario * cantidad) + (cantidad * valorUnitario * 0.19)
+            lista.append(subtotal)
+            print(f"SUBTOTAL: {subtotal} (IVA incluido)")
+            print(lista)
+            agregar = input("Faltan productos por agregar? S/N:")
+
+        elif iva == "N":
+            cantidad = int(input("Ingrese la cantidad del producto: "))
+            subtotal = (valorUnitario * cantidad)
+            lista.append(subtotal)
+            print(f"SUBTOTAL: {subtotal} (IVA incluido)")
+            print(lista)
+            agregar = input("Faltan productos por agregar? S/N:")
+        else:
+            agregar == "N"
+
+print(lista)
