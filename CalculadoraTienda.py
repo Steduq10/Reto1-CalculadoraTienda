9f3e2c36e7093bc1d440ed806280142729b05fb3
steduq10 <steven.duque.95@gmail.com>
2022-05-02 11:19:41 -0500

    Se configura el planteamiento inicial para el calculo del subtotal con iva y sin iva

--- CalculadoraTienda.py
+++ CalculadoraTienda.py
@@ -4,3 +4,14 @@ print("*****************************")
 
 valorUnitario = int(input("Por favor, ingrese el valor unitario del producto (sin IVA incluido): "))
 
+iva = input("¿El producto tiene IVA? S/N: ")
+
+while iva == "S":
+    cantidad = int(input("Ingrese la cantidad del producto: "))
+    subtotal = (valorUnitario*cantidad) + (cantidad*valorUnitario*0.19)
+    print(f"SUBTOTAL: {subtotal} (IVA incluido)")
+    
+while iva == "N":
+    cantidad = int(input("Ingrese la cantidad del producto: "))
+    subtotal = (valorUnitario * cantidad)
+    print(f"SUBTOTAL: {subtotal} (PRODUCTO SIN IVA)")
